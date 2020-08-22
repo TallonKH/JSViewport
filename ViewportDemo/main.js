c59c5f91aebc5320ca96aab5ef33a5e3b20aa75b
Tallon Hodge <tallonkh@gmail.com>
2020-08-21 23:38:35 -0400

    Various fixes

--- ViewportDemo/main.js
+++ ViewportDemo/main.js
@@ -8,43 +8,23 @@ $(function () {
 
 function setupElements() {
     rootDiv = document.getElementById("rootDiv");
+    viewport = new Viewport({
+        "minZoomFactor": 0.25,
+        "maxZoomFactor": 4,
+        "pannable": true,
+        "zoomSensitivity": 1,
+        "panSensitivity": 0.5,
+        "zoomCenter": "mouse"
+    });
+    viewport.background.color = "#1a1a1a"
+    rootDiv.appendChild(viewport.container);
 }
 
 function main() {
-    viewport = new CustomViewport();
-    rootDiv.appendChild(viewport.container);
-    viewport.background.color = "#1a1a1a";
-    // viewport.minZoomFactor = 0.25;
-    // viewport.maxZoomFactor = 2;
-    // viewport.pannable = true;
-    // viewport.zoomSensitivity = 1;
-    // viewport.panSensitivity = 0.5;
-    // viewport.zoomCenter = "mouse";
-    
     const grabbables = [];
     for (let i = -15; i < 15; i++) {
         const grabbable = new GrabObj(viewport, new NPoint(i*25*Math.cos(Math.abs(i) / 25), 0*Math.sin(i/10)));
         grabbable.color = colorLerp("#ff4747", "#4769ff", (i+15)/30);
         viewport.registerObj(grabbable);
-    }
-}
-
-class CustomViewport extends Viewport {
-    constructor({} = {}) {
-        super({
-            "minZoomFactor": 0.25,
-            "maxZoomFactor": 4,
-            "pannable": true,
-            "zoomSensitivity": 1,
-            "panSensitivity": 0.5,
-            "zoomCenter": "mouse"
-        });
-
-        this.backgroundColor = "#1a1a1a"
-    }
-
-    onRedraw () {
-        super.onRedraw();
-        // console.log("asdf");
     }
 }
